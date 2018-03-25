dbabff5b41880a2c82562b2312642807bcc59b6e
Sergio Lopez <sergoh@outlook.com>
2018-03-25 10:33:31 -0700

    Travis CI test 4

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ echo -e "$VARNAME"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "travis@travis-ci.org"
-        git config --global user.name "Travis"
+        git config --global user.email "sergoh@outlook.com"
+        git config --global user.name "sergoh"
     fi
     #using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
