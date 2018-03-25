97873b479c9101cab316604a7cbee4223fab0ae4
Sergio Lopez <sergoh@outlook.com>
2018-03-25 01:46:43 -0700

    Adding Authors and CName

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,6 +36,9 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 # Menu
 MAIN_MENU = True
 MENUITEMS = (('Archives', '/archives.html'),
